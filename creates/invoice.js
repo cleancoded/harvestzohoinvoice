51af307a38358482f3b96c8a37409e5cabedc787
Skye Griffin <skybox@trioptimum.com>
2018-12-22 06:58:42 -0600

    Adding minimum inputs to Invoice create

--- creates/invoice.js
+++ creates/invoice.js
@@ -14,7 +14,35 @@ module.exports = {
 
     operation: {
         inputFields: [
-
+            {
+                key: 'organization',
+                label: 'Organization',
+                required: true
+            },
+            {
+                key: 'line_item_id',
+                label: 'Line Item ID',
+                required: true,
+                helpText: 'The ID of Zoho Line Item to use'
+            },
+            {
+                key: 'contact_id',
+                label: 'Contact ID',
+                required: true,
+                helpText: 'The ID of the Zoho Contact being invoiced'
+            },
+            {
+                key: 'send',
+                label: 'Send',
+                required: false,
+                helpText: 'Should we send the invoice to the contact now?'
+            },
+            {
+                key: 'line_items',
+                label: 'Line Item Input',
+                required: true,
+                helpText: 'The digest content to be used as invoice line items'
+            }
         ],
         perform: createInvoice
     }
